948becd26e17d71c3dbffff5cc5e4ccbd421cfef
djjohns <djjohns@users.noreply.github.com>
2022-02-12 23:46:57 -0600

    Trying to keep the server open for multiple file uploads.

--- XferServer.py
+++ XferServer.py
@@ -37,32 +37,35 @@ class XferServer():
         # TODO: Un-compress file before sending.
         # TODO: Dencrypt file before sending.
         # TODO: Hashing an checksums of the files client and server.
-        s = socket.socket()  # Create our socket.
-        s.bind((self.host, self.port))  # Bind it to our host:port.
-        s.listen(10)  # Listen for a connection.
-        print(f"[*] Listening as {self.host}:{self.port}")
-        print("Waiting for the client to connect... ")
-        client_socket, address = s.accept()  # Accept the connection.
-        print(f"[+] {address} is connected.")
-        received = client_socket.recv(self.buffer_size).decode()  # Receive file over socket.
-        filename, filesize = received.split(self.separator)  # Split out filename and filesize.
-        # TODO: Set an explict path to write file to.
-        filename = os.path.basename(filename)  # Pass our filename and path to the writer.
-        filesize = int(filesize)  # Pass our filesize to the writer.
+        
+        while True:
+            s = socket.socket()  # Create our socket.
+            s.bind((self.host, self.port))  # Bind it to our host:port.
+            s.listen(10)  # Listen for a connection.
+            print(f"[*] Listening as {self.host}:{self.port}")
+            print("Waiting for the client to connect... ")
+            client_socket, address = s.accept()  # Accept the connection.
+            print(f"[+] {address} is connected.")
+            
+            received = client_socket.recv(self.buffer_size).decode()  # Receive file over socket.
+            filename, filesize = received.split(self.separator)  # Split out filename and filesize.
+            # TODO: Set an explict path to write file to.
+            filename = os.path.basename(filename)  # Pass our filename and path to the writer.
+            filesize = int(filesize)  # Pass our filesize to the writer.
 
-        # Fancy progress bar.
-        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-        # Write out file received in bytes updating progress bar.
-        # As well as closing both client and server sockets.
-        with open(filename, "wb") as f:
-            while True:
-                bytes_read = client_socket.recv(self.buffer_size)
-                if not bytes_read:
-                    break
-                f.write(bytes_read)
-                progress.update(len(bytes_read))
-        client_socket.close()
-        s.close()
+            # Fancy progress bar.
+            progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+            # Write out file received in bytes updating progress bar.
+            # As well as closing both client and server sockets.
+            with open(filename, "wb") as f:
+                while True:
+                    bytes_read = client_socket.recv(self.buffer_size)
+                    if not bytes_read:
+                        break
+                    f.write(bytes_read)
+                    progress.update(len(bytes_read))
+            client_socket.close()
+            s.close()
         
 
 
